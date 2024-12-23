15322fae488b57d3a700c45d2c7483fca25af2a1
Lab2k2 <77461736+Lab2k2@users.noreply.github.com>
2024-12-23 11:01:35 +0700

    Update llm_model.py

--- llm_model.py
+++ llm_model.py
@@ -3,7 +3,7 @@ import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from docx import Document as docx_document  # Đảm bảo cài đặt thư viện python-docx
 
-api_key = os.getenv("GOOGLE_API_KEY", "your_google_api")
+api_key = os.getenv("GOOGLE_API_KEY", "AIzaSyCy-F4waBhpZEwUeT5FH-ulfOT_0ySKOXw")
 os.environ["GOOGLE_API_KEY"] = api_key
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
